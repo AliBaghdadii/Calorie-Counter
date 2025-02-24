0d6dbe6b8af993e100e86b8137adf55db5aadee0
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-25 00:15:35 +0330

    Step 74

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function calculateCalories(e) {
     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
 
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
 }
 
 function getCaloriesFromInputs(list) {
